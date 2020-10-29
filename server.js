8dbb94db90d3990e85f52a91f4b899304970272a
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 14:52:16 -0600

    Organized and commented code

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
+// Dependencies
 const express = require('express');
-const app = express();
-
+const path = require('path');
+const fs = require('fs');
 const indexRouter = require('./routes/index');
 const notesRouter = require('./routes/notes');
 
-const path = require('path');
+// Express
+const app = express();
 
+// Array to hold notes
+const notesAll = require('./db/db.json')
+
+// dynamic port
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log('Listening on PORT: ' + PORT);
 });
 
-// view engine setup
+// Engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Routes
 app.use('', indexRouter);
 app.use('/notes', notesRouter);
-
 
 module.exports = app;
