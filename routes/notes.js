5eafcd8cb5665a931e619475aef6ed48f76e72f3
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 14:09:57 -0600

    rerouted notes.html

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET notes listing. */
-router.get('*', function (req, res) {
+router.get('/', function (req, res) {
   res.sendFile(__dirname + '/notes.html');
 });
 
