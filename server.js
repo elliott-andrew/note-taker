46218ed772f7c49d6f8f25373c03bd6f159dabe2
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 14:52:55 -0600

    removed unused path

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Dependencies
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
 const indexRouter = require('./routes/index');
 const notesRouter = require('./routes/notes');
