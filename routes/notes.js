9286b3ac185880e2291ed289ff104ced8f72f79a
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 13:37:30 -0600

    removed code not being used

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res) {
+/* GET notes listing. */
+router.get('*', function (req, res) {
   res.sendFile(__dirname + '/notes.html');
 });
 
--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var notesRouter = require('./routes/notes');
@@ -10,13 +7,11 @@ var notesRouter = require('./routes/notes');
 var app = express();
 
 // view engine setup
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('', indexRouter);
 app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
