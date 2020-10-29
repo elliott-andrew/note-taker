1c472b267014e7d699e19ddce4fbf2bb8b7774b5
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 13:59:47 -0600

    Intigrated bin and server.js. Removed unused code

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
-var path = require('path');
-
 var indexRouter = require('./routes/index');
 var notesRouter = require('./routes/notes');
 
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("Listening on PORT: " + PORT);
+});
 
 // view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('', indexRouter);
 app.use('/notes', notesRouter);
