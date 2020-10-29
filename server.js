001c7802621200005d53fcc254662cca1b35c21e
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-29 14:32:49 -0600

    Relinked style sheets from public folder

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var indexRouter = require('./routes/index');
 var notesRouter = require('./routes/notes');
-
+var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
-  console.log("Listening on PORT: " + PORT);
+  console.log('Listening on PORT: ' + PORT);
 });
 
 // view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('', indexRouter);
 app.use('/notes', notesRouter);
 
