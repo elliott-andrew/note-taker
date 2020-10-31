d73befecf4e85d1bdca502ddd3a248f9a5c20ee3
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-31 08:49:43 -0600

    Updated file structure

--- routes/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET notes listing. */
-router.get('*', function (req, res) {
-  res.sendFile(__dirname + '/notes.html');
-});
-
-module.exports = router;
