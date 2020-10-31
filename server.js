b990552312c35e68388df5adbf58d7d8913e8dec
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-31 09:50:16 -0600

    Fixed typo in app.post writeFile to send the data to the db file

--- server.js
+++ server.js
@@ -17,24 +17,41 @@ const PORT = process.env.PORT || 3000;
 // Engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Note Taking
 // ================================================================================
 // Get function
 app.get("/api/notes", function (req, res) {
+  // Read the notes array then parse the JSON data
   readFile(notesArray, "utf8").then(data => res.json(JSON.parse(data))).catch((err) => {
     console.log(err);
   });
 });
 
+// Generate IDs for notes
+function noteID(currentNotes) {
+  let highestId = 0;
+  currentNotes.forEach(note => {
+    if (note.id > highestId) {
+      highestId = note.id;
+    }
+  });
+  return highestId + 1;
+};
+
 // Post function
 app.post("/api/notes", function (req, res) {
+  // read the notes array file
   readFile(notesArray, "utf8").then(data => {
+    // add the JSON parsed data to the variable notes
     let notes = JSON.parse(data);
-    const newNote = { ...req.body };
+    // Use the spread operator to add the submitted note to a new variable and assign it an ID we can later reference.
+    const newNote = { ...req.body, "id": noteID(notes) };
+    // add the new note to the notes variable.
     notes.push(newNote);
-    writeFile(noteArray, JSON.stringify(notes)).then(() => {
+    // write the db file
+    writeFile(notesArray, JSON.stringify(notes)).then(() => {
       res.json(newNote);
     })
       .catch((err) => {
@@ -48,11 +65,13 @@ app.post("/api/notes", function (req, res) {
 
 // Delete function
 app.delete("/api/notes/:id", function (req, res) {
+  // Grab the ID of the note clicked
   const deletedNote = parseInt(req.params.id);
-  readFile(noteArray, "utf8").then(data => {
+  readFile(notesArray, "utf8").then(data => {
     let notes = JSON.parse(data);
+    // filter the notes and find the ID of the selected note, send the request
     notes = notes.filter(newNote => newNote.id !== deletedNote);
-    writeFile(noteArray, JSON.stringify(notes)).then(() => {
+    writeFile(notesArray, JSON.stringify(notes)).then(() => {
       res.send('Got a DELETE request at /user');
     })
       .catch((err) => {
@@ -81,6 +100,3 @@ app.get("*", function (req, res) {
 app.listen(PORT, function () {
   console.log('Listening on PORT: ' + PORT);
 });
-
-// Export
-module.exports = app;
